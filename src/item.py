e5cfbbbb16de1b2b67e9565581886d1ea8f6edfc
Igor Barbur <Igor.barbur@yandex.ru>
2024-01-30 18:25:28 +0800

    Implementation of a method class, a static method and a “property” with a “setter”.

--- src/item.py
+++ src/item.py
@@ -1,3 +1,7 @@
+import os
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,7 +17,7 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
@@ -31,3 +35,35 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price = self.price * Item.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls, path):
+        """
+        Класс-метод, инициализирующий экземпляры класса 'Item' данными из файла 'src/items.csv.
+        """
+        Item.all = []
+        path = os.path.join(os.path.dirname(__file__), 'items.csv')
+        with open(path, 'r', newline='\n', encoding='windows- 1251') as file:
+            reader = csv.DictReader(file)
+            lines = list(reader)
+            for line in lines:
+                print(cls(name=line.get('name'),
+                          price=line.get('price'),
+                          quantity=line.get('quantity')))
+
+    @staticmethod
+    def string_to_number(name):
+        return int(float(name))
+
+    @property
+    def name(self):
+        """Получаем возможность обращаться к '__name' вне класса"""
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        """Вносим изменения в '__name'"""
+        if len(name) > 10:
+            print('Длина наименования товара превышает 10 символов')
+        else:
+            self.__name = name
