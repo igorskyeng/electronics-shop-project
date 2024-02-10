408e07aa9473283ea31ec7cac7ee2c3c8083e27a
Igor Barbur <Igor.barbur@yandex.ru>
2024-02-10 20:04:26 +0800

    Homework 5 ver-2

--- src/keyboard.py
+++ src/keyboard.py
@@ -2,10 +2,17 @@ from src.item import Item
 
 
 class MixinLog:
+    """Lополнительный функционал по хранению и изменению раскладки клавиатуры для класса 'Keyboard'."""
     def __init__(self, language="EN"):
+        """
+        :__language: Атрибут языка клавиатеры.
+        """
         self.__language = language
 
     def change_lang(self):
+        """
+        Смена языка клавиатуры.
+        """
         self.__language = "RU" if self.__language == "EN" else "EN"
 
     @property
@@ -14,6 +21,7 @@ class MixinLog:
 
 
 class Keyboard(Item, MixinLog):
+    """Класс для товара “клавиатура”."""
     def __init__(self, name, price, quantity):
         super().__init__(name, price, quantity)
         MixinLog.__init__(self)
